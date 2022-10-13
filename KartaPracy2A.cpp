0442e1c4f9e9ff3c8dccddef43e67a17a2495d60
PerzotProgramer <bartoszperz2007@gmail.com>
2022-10-13 21:25:19 +0200

    Dodanie zad1

--- KartaPracy2A.cpp
+++ KartaPracy2A.cpp
@@ -1,6 +1,13 @@
 #include<iostream>
-using namespace std;
+using std::cout, std::cin, std::endl;
 int main()
 {
-    //WSZYSTKO TODO
+    int a,b,c,d;
+    
+    cout<<"ZADANIE1"<<endl;
+    cout<<"Podaj liczbe1: ";  cin>>a;
+    cout<<"Podaj liczbe2: ";  cin>>b;
+    if ((a + b) % 2 == 0) cout<<"TAK, suma jest parzysta";
+    else cout<<"NIE, suma nie jest parzysta";
+
 }
